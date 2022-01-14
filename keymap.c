493d7131d54de896f58eebabc02f86d790226eb1
jeremy <jliverson5@gmail.com>
2022-01-13 19:47:39 -0600

    Update keymap

--- keymap.c
+++ keymap.c
@@ -38,11 +38,11 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * `-----------------------------------------------------------------------------------'
    */
   [BASE] = LAYOUT_preonic_grid(
-    KC_1,          KC_2,             KC_3,             KC_4,             KC_5, KC_NO, KC_NO,             KC_6,             KC_7,         KC_8,         KC_9, KC_0,
-    KC_Q,          KC_W,             KC_E,             KC_R,             KC_T, KC_NO, KC_NO,             KC_Y,             KC_U,         KC_I,         KC_O, KC_P,
-    LGUI_T(KC_A),  LALT_T(KC_S),     LCTL_T(KC_D),     LSFT_T(KC_F),             KC_G, KC_NO, KC_NO,             KC_H,     LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN),
-    KC_Z,          KC_X,             KC_C,             KC_V,             KC_B, KC_NO, KC_NO,             KC_N,             KC_M,      KC_COMM,       KC_DOT, KC_SLSH,
-    KC_NO,        KC_NO, LT(MEDR, KC_ESC), LT(NAVR, KC_TAB), LT(MOUR, KC_SPC), KC_NO, KC_NO, LT(NSSL, KC_ENT), LT(NSL, KC_BSPC),       KC_DEL,        KC_NO, KC_NO
+    KC_1,          KC_2,             KC_3,             KC_4,             KC_5,             KC_NO, KC_NO,             KC_6,             KC_7,         KC_8,         KC_9,            KC_0,
+    KC_Q,          KC_W,             KC_E,             KC_R,             KC_T,             KC_NO, KC_NO,             KC_Y,             KC_U,         KC_I,         KC_O,            KC_P,
+    LGUI_T(KC_A),  LALT_T(KC_S),     LCTL_T(KC_D),     LSFT_T(KC_F),     KC_G,             KC_NO, KC_NO,             KC_H,     LSFT_T(KC_J), LCTL_T(KC_K), LALT_T(KC_L), LGUI_T(KC_SCLN),
+    KC_Z,          KC_X,             KC_C,             KC_V,             KC_B,             KC_NO, KC_NO,             KC_N,             KC_M,      KC_COMM,       KC_DOT,         KC_SLSH,
+    KC_NO,         KC_NO,            LT(MEDR, KC_ESC), LT(NAVR, KC_TAB), LT(MOUR, KC_SPC), KC_NO, KC_NO, LT(NSSL, KC_ENT), LT(NSL, KC_BSPC),       KC_DEL,        KC_NO,            KC_NO
   ),
 
   /* Navigation (right-hand)
@@ -51,9 +51,9 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |  N/A |  N/A | U_RDO| U_PST| U_CPY| U_CUT| U_UND|
    * |------+------+------+------+------+-------------+------+------+------+------+------|
-   * |      |      |      |      |      |  N/A |  N/A | CAPS | LEFT | DOWN |  UP  | RGHT |
+   * |      |      |      |      |      |  N/A |  N/A | LEFT | DOWN | UP   | RGHT | INS  |
    * |------+------+------+------+------+------|------+------+------+------+------+------|
-   * |      |      |      |      |      |  N/A |  N/A | INS  | HOME | PGDN | PGUP | END  |
+   * |      |      |      |      |      |  N/A |  N/A | CAPS | HOME | PGDN | PGUP | END  |
    * |------+------+------+------+------+------+------+------+------+------+------+------|
    * |      |      |      |      |      |  N/A |  N/A |  ENT | BSPC |  DEL |  N/A |  N/A |
    * `-----------------------------------------------------------------------------------'
@@ -61,8 +61,8 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
   [NAVR] = LAYOUT_preonic_grid(
     _______, _______, _______, _______, _______, KC_NO, KC_NO, _______, _______, _______, _______, _______,
     _______, _______, _______, _______, _______, KC_NO, KC_NO, KC_AGIN, KC_PSTE, KC_COPY,  KC_CUT, KC_UNDO,
-    _______, _______, _______, _______, _______, KC_NO, KC_NO, KC_CAPS, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,
-    _______, _______, _______, _______, _______, KC_NO, KC_NO,  KC_INS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,
+    _______, _______, _______, _______, _______, KC_NO, KC_NO, KC_LEFT, KC_DOWN,   KC_UP, KC_RGHT,  KC_INS,
+    _______, _______, _______, _______, _______, KC_NO, KC_NO, KC_CAPS, KC_HOME, KC_PGDN, KC_PGUP,  KC_END,
     _______, _______, _______, _______, _______, KC_NO, KC_NO,  KC_ENT, KC_BSPC,  KC_DEL,   KC_NO,   KC_NO
   ),
 
